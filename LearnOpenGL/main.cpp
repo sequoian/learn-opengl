c608c0462a11b20ac71cfbab2e9776d919ea4569
Zach <zach303@msn.com>
2018-08-13 23:41:00 -0700

    fixed vertex shader

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -22,6 +22,7 @@ const unsigned int SRC_HEIGHT = 600;
 
 const char* vertexShaderSource = 
 	"#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;"
 	"void main()\n"
 	"{\n"
 	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
